25991e79f502eb62b7d69af29d66fd2dc6ace7b7
Luca <sorrentino.luca@outlook.com>
2020-05-05 21:34:10 +0200

    terminata l' app

--- App/PopulateListview/app/src/main/java/com/androdocs/populatelistview/CustomAdapter.kt
+++ App/PopulateListview/app/src/main/java/com/androdocs/populatelistview/CustomAdapter.kt
@@ -1,17 +1,23 @@
 package com.androdocs.populatelistview
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import com.squareup.picasso.Picasso
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
 
 class CustomAdapter(private val context: Context,
                     private val dataList: ArrayList<HashMap<String, String>>) : BaseAdapter() {
 
     private val inflater: LayoutInflater = this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    val client = OkHttpClient()
+
     override fun getCount(): Int { return dataList.size }
     override fun getItem(position: Int): Int { return position }
     override fun getItemId(position: Int): Long { return position.toLong() }
@@ -26,27 +32,26 @@ class CustomAdapter(private val context: Context,
 
         Picasso.get()
             .load(dataitem.get("image"))
-            .resize(50, 50)
+            .resize(70, 70)
             .centerCrop()
             .into(rowView.findViewById<ImageView>(R.id.row_image))
 
         val delete_button =  rowView.findViewById<Button>(R.id.delete)
 
         delete_button.setOnClickListener{
-            Toast.makeText(rowView.getContext(), "Cibo Rimosso", Toast.LENGTH_LONG).show()
+
+
+
+            val request = Request.Builder()
+                                .url(HttpUrl.parse(dataitem.get("callback")))
+                                .delete()
+                                .build()
+
+            val response = client.newCall(request).execute()
+            Toast.makeText(rowView.getContext(), "Rimosso " + dataitem.get("name").toString(), Toast.LENGTH_LONG).show()
             dataList.remove(dataList[position])
             notifyDataSetChanged()
         }
-
-
-        val to_shoplist =  rowView.findViewById<Button>(R.id.to_shoplist)
-
-        to_shoplist.setOnClickListener{
-            Toast.makeText(rowView.getContext(), "Cibo Aggiunto Al Frigo", Toast.LENGTH_LONG).show()
-            dataList.remove(dataList[position])
-            notifyDataSetChanged()
-        }
-
 
 
         rowView.tag = position
--- App/PopulateListview/app/src/main/java/com/androdocs/populatelistview/MainActivity.kt
+++ App/PopulateListview/app/src/main/java/com/androdocs/populatelistview/MainActivity.kt
@@ -2,22 +2,25 @@ package com.androdocs.populatelistview
 
 import android.os.AsyncTask
 import android.os.Bundle
-import android.system.Os.bind
+import android.os.StrictMode
 import android.view.View
-import android.widget.Button
 import android.widget.ListView
 import android.widget.ProgressBar
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
 import java.net.URL
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     var dataList = ArrayList<HashMap<String, String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val policy =
+            StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -32,7 +35,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun doInBackground(vararg params: String?): String? {
-            return URL("https://www.androdocs.com/files/uploads/original/sample-json-data-1567767983.txt").readText(
+            //String url = "https://www.androdocs.com/files/uploads/original/sample-json-data-1567767983.txt";
+            return URL("https://smartfridge-app.herokuapp.com/shoplist/getItems").readText(
                     Charsets.UTF_8
                 )
         }
@@ -42,20 +46,32 @@ class MainActivity : AppCompatActivity() {
             findViewById<ProgressBar>(R.id.loader).visibility = View.GONE
 
             val jsonObj = JSONObject(result)
-            val usersArr = jsonObj.getJSONArray("users")
-            for (i in 0 until usersArr.length()) {
-                val singleUser = usersArr.getJSONObject(i)
 
-                val map = HashMap<String, String>()
-                map["name"] = singleUser.getString("name")
-                map["age"] = singleUser.getString("age")
-                map["city"] = singleUser.getString("city")
-                map["image"] = singleUser.getString("image")
-                dataList.add(map)
+
+
+
+            // Debug JSON
+            val TAG = MainActivity::class.java.simpleName
+
+
+            val keys: Iterator<String> = jsonObj.keys()
+
+            while (keys.hasNext()) {
+                val key = keys.next()
+                if (jsonObj.get(key) is JSONObject) {
+                    val item = (jsonObj.get(key) as JSONObject).getJSONObject("shopitem")
+                    val map = HashMap<String, String>()
+                    map["name"] = item.get("name").toString()
+                    map["age"] = item.get("notes").toString()
+                    map["city"] = item.get("category").toString()
+                    map["image"] = item.get("url_img").toString()
+                    val url_back = (jsonObj.get(key) as JSONObject).get("delete_callback")
+                    map["callback"] = url_back.toString()
+                    dataList.add(map)
+                }
             }
 
             findViewById<ListView>(R.id.listView).adapter = CustomAdapter(this@MainActivity, dataList)
-
 
         }
 
